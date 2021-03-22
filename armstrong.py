30130df9fe30c67d1b05e573120fbc9a0c94d08d
ramsharma212 <80679513+ramsharma212@users.noreply.github.com>
2021-03-22 15:53:28 +0530

    the number is Armstrong or not

---
+++ armstrong.py
@@ -0,0 +1,10 @@
+a = input("enter the number")
+ln = len(a)
+s = 0
+for i in a:
+    s += int(i)**ln
+if int(a) == s:
+    print("number is armstrong")
+else:
+    print("number is not armstrong")
+    
